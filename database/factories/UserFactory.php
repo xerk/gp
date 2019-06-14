e91cb78bfd65e3ec81d29a895f27cf39e9150158
Ahmed Mamdouh <gm.xerk@gmail.com>
2019-06-14 21:47:17 +0200

    2

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -28,7 +28,7 @@ $factory->define(User::class, function (Faker $faker) {
         'email_verified_at' => now(),
         'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
         'remember_token' => Str::random(10),
-        'job' => 0,
+        'job' => 1,
         'gender' => array_rand($strings),
         'phone' => $faker->e164PhoneNumber,
         'avatar' => Arr::random($avatar),
